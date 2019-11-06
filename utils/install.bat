afed9c1a8e1f200c7e838084086e4644c52d57f1
Bootscreen <Bootscreen@users.noreply.github.com>
2019-11-06 14:31:06 +0100

    Update install.bat
    With %APPDATA% its not needed to update the username

--- utils/install.bat
+++ utils/install.bat
@@ -5,7 +5,6 @@ cd /d %~dp0
 cd %1
 
 REM MAKE SURE THE FOLLOWING ARE CORRECT
-REM ALSO, UPDATE YOUR_USERNAME ON LINE 16
 SET OUTPUT_DIR="C:\TEMP"
 SET DISTRIBUTION_TOOL="e:\Projects\DotNet\Stream Deck Distribution\DistributionTool.exe"
 SET STREAM_DECK_FILE="D:\Program Files\Elgato\StreamDeck\StreamDeck.exe"
@@ -15,7 +14,7 @@ taskkill /f /im %2.exe
 timeout /t 2
 del %OUTPUT_DIR%\%2.streamDeckPlugin
 %DISTRIBUTION_TOOL% %2.sdPlugin %OUTPUT_DIR%
-rmdir C:\Users\YOUR_USERNAME\AppData\Roaming\Elgato\StreamDeck\Plugins\%2.sdPlugin /s /q
+rmdir %APPDATA%\Elgato\StreamDeck\Plugins\%2.sdPlugin /s /q
 START "" %STREAM_DECK_FILE%
 timeout /t 3
 %OUTPUT_DIR%\%2.streamDeckPlugin
