73f1400c3bd0be691d1099c2b1ce68e619ad3d94
BarRaider <46548278+BarRaider@users.noreply.github.com>
2023-02-05 11:34:09 +0200

    Support JObject in SetFeedback

---
+++ barraider-sdtools/Communication/Messages/SetFeedbackMessageEx.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace BarRaider.SdTools.Communication.Messages
+{
+    internal class SetFeedbackMessageEx : IMessage
+    {
+        [JsonProperty("event")]
+        public string Event { get { return "setFeedback"; } }
+
+        [JsonProperty("context")]
+        public string Context { get; private set; }
+
+        [JsonProperty("payload")]
+        public JObject Payload { get; private set; }
+
+        public SetFeedbackMessageEx(JObject payload, string pluginUUID)
+        {
+            this.Context = pluginUUID;
+            Payload = payload;
+        }
+    }
+}
